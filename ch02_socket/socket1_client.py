959eceb26af73cb717ad63015b3459512f1740f8
shaoling <sshling@gmail.com>
2014-07-12 19:02:18 +0800

    t

--- ch02_socket/socket1_client.py
+++ ch02_socket/socket1_client.py
@@ -1,11 +1,12 @@
 import socket
 from time import ctime  
-if "__main__" == __name__:
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect(('localhost',8080))
-    sendStr=" shaoling2"
-    sock.send(sendStr.encode())
-    recv = sock.recv(1024).decode()
-    print(recv)
-    sock.close()
-    print(ctime()+ ' client :end of the connecct')
+
+#UDP : socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.connect(('localhost',8080))
+sendStr=" shaoling2"
+sock.send(sendStr.encode())
+recv = sock.recv(1024).decode()
+print(recv)
+sock.close()
+print(ctime()+ ' client :end of the connecct')
